845937c574d53e7e1b968bc5a8d34707b187295c
Antony Holmes <antony@antonyholmes.dev>
2025-12-19 12:49:58 -0500

    fix: Migration of routes to packages

--- go.mod
+++ go.mod
@@ -11,28 +11,53 @@ replace github.com/antonybholmes/go-basemath => ../go-basemath
 require (
 	github.com/antonybholmes/go-basemath v0.0.0-20251211184815-6e7285b975dd
 	github.com/antonybholmes/go-sys v0.0.0-20251215211105-61a3d71011f7
+	github.com/gin-gonic/gin v1.11.0
 	github.com/mattn/go-sqlite3 v1.14.32
 )
 
 require (
+	github.com/bytedance/gopkg v0.1.3 // indirect
+	github.com/bytedance/sonic v1.14.2 // indirect
+	github.com/bytedance/sonic/loader v0.4.0 // indirect
+	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.11 // indirect
+	github.com/gin-contrib/sse v1.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.28.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/matoous/go-nanoid/v2 v2.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/quic-go/qpack v0.6.0 // indirect
+	github.com/quic-go/quic-go v0.57.1 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.4 // indirect
 	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/tiendc/go-deepcopy v1.7.2 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.3.1 // indirect
 	github.com/xuri/efp v0.0.1 // indirect
 	github.com/xuri/excelize/v2 v2.10.0 // indirect
 	github.com/xuri/nfp v0.0.2-0.20250530014748-2ddeb826f9a9 // indirect
+	golang.org/x/arch v0.23.0 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/exp v0.0.0-20251209150349-8475f28825e9 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
 
 require (
 	github.com/antonybholmes/go-dna v0.0.0-20251215211059-b3acf082663a
+	github.com/antonybholmes/go-web v0.0.0-20251215211100-5555b69aa3c0
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.39.0 // indirect
---
+++ routes/routes.go
@@ -0,0 +1,173 @@
+package routes
+
+import (
+	"errors"
+
+	"github.com/antonybholmes/go-dna"
+	seq "github.com/antonybholmes/go-seqs"
+	"github.com/antonybholmes/go-seqs/seqsdbcache"
+	"github.com/antonybholmes/go-sys/log"
+	"github.com/antonybholmes/go-web"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	ErrNoGenomeSupplied = errors.New("must supply a genome")
+)
+
+type (
+	ReqSeqParams struct {
+		Locations []string `json:"locations"`
+		Scale     float64  `json:"scale"`
+		BinSizes  []int    `json:"binSizes"`
+		Tracks    []string `json:"tracks"`
+	}
+
+	SeqParams struct {
+		Locations []*dna.Location
+		Scale     float64
+		BinSizes  []int
+		Tracks    []string
+	}
+
+	SeqResp struct {
+		Location *dna.Location         `json:"location"`
+		Tracks   []*seq.TrackBinCounts `json:"tracks"`
+	}
+)
+
+func ParseSeqParamsFromPost(c *gin.Context) (*SeqParams, error) {
+
+	var params ReqSeqParams
+
+	err := c.Bind(&params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	locations := make([]*dna.Location, 0, len(params.Locations))
+
+	for _, loc := range params.Locations {
+		location, err := dna.ParseLocation(loc)
+
+		if err != nil {
+			return nil, err
+		}
+
+		locations = append(locations, location)
+	}
+
+	return &SeqParams{
+			Locations: locations,
+			BinSizes:  params.BinSizes,
+			Tracks:    params.Tracks,
+			Scale:     params.Scale},
+		nil
+}
+
+func GenomeRoute(c *gin.Context) {
+	platforms, err := seqsdbcache.Genomes()
+
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	web.MakeDataResp(c, "", platforms)
+}
+
+func PlatformRoute(c *gin.Context) {
+	genome := c.Param("assembly")
+
+	platforms, err := seqsdbcache.Platforms(genome)
+
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	web.MakeDataResp(c, "", platforms)
+}
+
+func TracksRoute(c *gin.Context) {
+	platform := c.Param("platform")
+	genome := c.Param("assembly")
+
+	tracks, err := seqsdbcache.Tracks(platform, genome)
+
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	web.MakeDataResp(c, "", tracks)
+}
+
+func SearchSeqRoute(c *gin.Context) {
+	genome := c.Param("assembly")
+
+	if genome == "" {
+		web.BadReqResp(c, ErrNoGenomeSupplied)
+		return
+	}
+
+	query := c.Query("search")
+
+	tracks, err := seqsdbcache.Search(genome, query)
+
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	web.MakeDataResp(c, "", tracks)
+}
+
+func BinsRoute(c *gin.Context) {
+
+	params, err := ParseSeqParamsFromPost(c)
+
+	if err != nil {
+		log.Debug().Msgf("err %s", err)
+		c.Error(err)
+		return
+	}
+
+	//log.Debug().Msgf("bin %v %v", params.Locations, params.BinSizes)
+
+	ret := make([]*SeqResp, 0, len(params.Locations)) //make([]*seq.BinCounts, 0, len(params.Tracks))
+
+	for li, location := range params.Locations {
+		resp := SeqResp{Location: location, Tracks: make([]*seq.TrackBinCounts, 0, len(params.Tracks))}
+
+		for _, track := range params.Tracks {
+			reader, err := seqsdbcache.ReaderFromId(track,
+				params.BinSizes[li],
+				params.Scale)
+
+			if err != nil {
+				//log.Debug().Msgf("stupid err %s", err)
+				c.Error(err)
+				return
+			}
+
+			// guarantees something is returned even with error
+			// so we can ignore the errors for now to make the api
+			// more robus
+			trackBinCounts, _ := reader.TrackBinCounts(location)
+
+			// if err != nil {
+			// 	return web.ErrorReq(err)
+			// }
+
+			resp.Tracks = append(resp.Tracks, trackBinCounts)
+		}
+
+		ret = append(ret, &resp)
+	}
+
+	//log.Debug().Msgf("ret %v", len(ret))
+
+	web.MakeDataResp(c, "", ret)
+}
